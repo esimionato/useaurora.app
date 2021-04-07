0f1901197f32f2f774c49e6bb0024e5f90379c62
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-08 01:24:32 +0200

    Added OG Data

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,19 @@ const Index = () => (
         name="description"
         content="Aurora is the Open Source Cookieless Analytics Platform - Designed for Humans & Developers"
       />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content="https://www.useaurora.app" />
+      <meta name="twitter:site" content="@itsrennyman" />
+      <meta name="twitter:creator" content="@itsrennyman" />
+      <meta name="twitter:title" content="Use Aurora!" />
+      <meta
+        name="twitter:description"
+        content="Open Source Cookieless Analytics Platform - Designed for Humans & Developers"
+      />
+      <meta
+        name="twitter:image:src"
+        content="https://www.useaurora.app/og.png"
+      />
     </Head>
 
     <Hero />
