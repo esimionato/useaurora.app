4f2e18bbc391cd82d5656b1174a3d3c22bc90483
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-08 01:12:23 +0200

    Some CSS Fix

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -2,26 +2,23 @@ const CallToAction = () => (
   <div className="mt-4 md:mt-10 lg:mt-20">
     <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
       <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-        <span className="block">Ready to dive in?</span>
-        <span className="block text-indigo-600">
-          Start your free trial today.
-        </span>
+        <span className="block">Coming Soon..</span>
+        <span className="block text-blue-600">Get in touch for News!</span>
       </h2>
       <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
         <div className="inline-flex rounded-md shadow">
-          <a
-            href="#"
-            className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-          >
-            Get started
-          </a>
-        </div>
-        <div className="ml-3 inline-flex rounded-md shadow">
-          <a
-            href="#"
-            className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
-          >
-            Learn more
+          <a href="https://twitter.com/itsrennyman" className="group flex">
+            <div className="w-full sm:w-auto inline-flex items-center justify-between text-white font-medium bg-blue-400 bg-opacity-100 group-hover:bg-opacity-70 rounded-lg shadow-sm group-hover:shadow-lg py-3 px-5 border border-white border-opacity-10 transform group-hover:-translate-y-0.5 transition-all duration-150">
+              <svg
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="text-white opacity-40 mr-3"
+              >
+                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"></path>
+              </svg>
+              <span>Get in Touch!</span>
+            </div>
           </a>
         </div>
       </div>
--- components/Features.js
+++ components/Features.js
@@ -16,7 +16,7 @@ const Features = () => (
       <div className="mt-16">
         <div className="md:grid md:grid-cols-2 md:gap-10 lg:grid-cols-3">
           <div>
-            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-indigo-600 to-blue-600 text-white">
+            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-blue-600 to-blue-600 text-white">
               <svg className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z"></path>
               </svg>
@@ -32,7 +32,7 @@ const Features = () => (
             </div>
           </div>
           <div className="mt-10 md:mt-0">
-            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-indigo-600 to-blue-600 text-white">
+            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-blue-600 to-blue-600 text-white">
               <svg className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                 <path
                   fillRule="evenodd"
@@ -52,7 +52,7 @@ const Features = () => (
             </div>
           </div>
           <div className="mt-10 md:mt-0">
-            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-indigo-600 to-blue-600 text-white">
+            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-blue-600 to-blue-600 text-white">
               <svg className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                 <path
                   fillRule="evenodd"
@@ -72,7 +72,7 @@ const Features = () => (
             </div>
           </div>
           <div className="mt-10 md:mt-0">
-            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-indigo-600 to-blue-600 text-white">
+            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-blue-600 to-blue-600 text-white">
               <svg className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
               </svg>
@@ -88,7 +88,7 @@ const Features = () => (
             </div>
           </div>
           <div className="mt-10 md:mt-0">
-            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-indigo-600 to-blue-600 text-white">
+            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-blue-600 to-blue-600 text-white">
               <svg className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
               </svg>
@@ -104,7 +104,7 @@ const Features = () => (
             </div>
           </div>
           <div className="mt-10 md:mt-0">
-            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-indigo-600 to-blue-600 text-white">
+            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-blue-600 to-blue-600 text-white">
               <svg className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
               </svg>
@@ -119,7 +119,7 @@ const Features = () => (
             </div>
           </div>
           <div className="mt-10 md:mt-0">
-            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-indigo-600 to-blue-600 text-white">
+            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-blue-600 to-blue-600 text-white">
               <svg className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                 <path
                   fillRule="evenodd"
@@ -139,7 +139,7 @@ const Features = () => (
             </div>
           </div>
           <div className="mt-10 md:mt-0">
-            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-indigo-600 to-blue-600 text-white">
+            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-blue-600 to-blue-600 text-white">
               <svg className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                 <path
                   fillRule="evenodd"
@@ -159,7 +159,7 @@ const Features = () => (
             </div>
           </div>
           <div className="mt-10 md:mt-0">
-            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-indigo-600 to-blue-600 text-white">
+            <div className="flex items-center justify-center h-12 w-12 rounded-md bg-gradient-to-r from-blue-600 to-blue-600 text-white">
               <svg className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"></path>
               </svg>
