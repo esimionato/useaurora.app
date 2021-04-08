63b1fd13108f79946582bb3aebf8d58d7aac025d
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-08 19:01:29 +0200

    Added aurora script

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script
+            async
+            defer
+            src="https://demo.useaurora.app/aurora.js"
+            aurora-id="f3d811716007dcfbbec4c155d00a30dc"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
