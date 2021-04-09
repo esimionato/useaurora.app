4f72e88e61204c8f6732d9caa51d732ea54b0a67
Renato Pozzi <r.pozzi@bluecube.it>
2021-04-09 19:53:10 +0200

    Fixed Homepage

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -2,7 +2,7 @@ const CallToAction = () => (
   <div className="mt-4 md:mt-10 lg:mt-20">
     <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
       <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-        <span className="block">Coming Soon..</span>
+        <span className="block">Sounds Great?</span>
         <span className="block text-blue-600">Get in touch for News!</span>
       </h2>
       <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
--- components/Hero.js
+++ components/Hero.js
@@ -16,18 +16,44 @@ const Hero = () => (
           The Open Source Analytics Platform - Made for Humans &amp; Developers.
         </p>
 
-        <div className="flex justify-center mt-20">
-          <a href="https://twitter.com/itsrennyman" className="group flex">
+        <div className="flex justify-center mt-20 space-x-4">
+          <a
+            href="https://github.com/itsrennyman/aurora"
+            className="group flex"
+          >
             <div className="w-full sm:w-auto inline-flex items-center justify-between text-white font-medium bg-white bg-opacity-20 group-hover:bg-opacity-30 rounded-lg shadow-sm group-hover:shadow-lg py-3 px-5 border border-white border-opacity-10 transform group-hover:-translate-y-0.5 transition-all duration-150">
               <svg
-                width="20"
-                height="20"
+                width="24"
+                height="24"
                 fill="currentColor"
                 className="text-white opacity-40 mr-3"
               >
-                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84"></path>
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z"
+                ></path>
               </svg>
-              <span>Get in touch for news!</span>
+
+              <span>Get Started!</span>
+            </div>
+          </a>
+          <a
+            href="https://demo.useaurora.app/s/f3d811716007dcfbbec4c155d00a30dc"
+            className="group flex"
+          >
+            <div className="w-full sm:w-auto inline-flex items-center justify-between text-white font-medium bg-blue-400 bg-opacity-100 group-hover:bg-opacity-70 rounded-lg shadow-sm group-hover:shadow-lg py-3 px-5 border border-white border-opacity-10 transform group-hover:-translate-y-0.5 transition-all duration-150">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                className="text-white opacity-40 mr-3"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z" />
+              </svg>
+              <span>See The Demo!</span>
             </div>
           </a>
         </div>
